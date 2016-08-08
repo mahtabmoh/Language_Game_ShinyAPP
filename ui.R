bd44d19bfafc1a1e5017d4a789133267f2a4f0af
mahtabmoh <mahtab.moh@gmail.com>
2016-08-08 13:06:48 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,4 +1,11 @@
 
+
+
+#reactiveBar <- function (outputId) 
+#{
+#  HTML(paste("<div id=\"", outputId, "\" class=\"shiny-lg-output\"><svg /></div>", sep=""))
+#}
+
 shinyUI(fluidPage(
   titlePanel(h1("Language Game")),
   
@@ -10,36 +17,57 @@ shinyUI(fluidPage(
   #         style="width: 50px; height :50px; left: 100px; top: 100px;
    #        background-color: gray; position: absolute"),
   
+  # Create the layout of the side bar,
+  # Create the parameters, all of which are inputed by user
   sidebarLayout(
     sidebarPanel(
       h2("Parameters"),
-      checkboxInput("wealth.reset", label = h5("Reset wealth of replicated organisms"),value = FALSE),
+      checkboxInput("wealth_reset", label = h5("Reset wealth of replicated organisms"),value = FALSE),
       actionButton("start", label = h5("Start")),
       actionButton("reset", label = h5("Reset")),
-      sliderInput("n", label = h5("Number of organisms"), min = 50, max = 200, value = 100,step = 50),
-      sliderInput("iter", label = h5("Number of Iterations"), min = 10, max = 500,value = 200, step = 10),
+      sliderInput("n", label = h5("Number of organisms"), min = 50, max = 200, value = 50,step = 50),
+      sliderInput("iter", label = h5("Number of Iterations"), min = 10, max = 500,value = 50, step = 10),
       sliderInput("memspan", label = h5("Memory Span"),min = 1,max = 50,value = 5, step = 5),
       sliderInput("dial_change_rate", label = h5("Dialect Update Probability (percentage)"), min = 0, max = 100, value = 1, step= 10),
-      sliderInput("repr_rate", label = h5("Reproduction Rate"), min= 1, max= 50, value = 20, step = 1),
+      sliderInput("repr_rate", label = h5("Reproduction Rate"), min= 1, max= 50, value = 10, step = 5),
       sliderInput("Beta", label = h5("Decay Factor"), min = 1, max = 1.05, value = 1.029), step = 0.005),
-      
+    
+    # Create the main panel in which the plots are rendered
+    # Each plot is rendered in a seperate tab
+    # Define the output variable for the plots to be rendered
     mainPanel(
       position = "left",
       h3("About the Game"),
       h4("This game simulates interactions between the members of a population of organisms that are assigned some randome characteristics.
         Organims are distributed randomly on a linear space, and interact with their neighbours.
         The nature of the interactions is defined by the strategies of organisms."),
-      p("Click on the start button: The plot below shows the evolution of the species over iterations, and thus the dialect that evolves during the course of time."),
-      plotlyOutput("evolu"),
-      
-      # Creating tags to pass to .js
+      h5("(Navigate between the tabs to view static/dynamic plots.)"),
+      h5("(Due to the large size of the output dataset rendering might take several minutes.)"),
+      tabsetPanel(
+        tabPanel('Encounters Plot, click Start',
+                 helpText("Hover over each point to show the ID of the corresponding organism"),br(),
+                 plotlyOutput("encounter")),
+                
+        tabPanel('gvis Motion Chart, click Start',
+                 helpText("This plot animates the abundance of the species over cycles and the convergence
+                          of the population towards one strategy.
+                          Adjust the speed of motion next to play button. Select x-axis, y-axis and 
+                          size variable. Select log or lin for x or y-axis variable. Select unique colors from the color
+                          slide bar to distinct the colors of each species.Navigate between
+                          tabs at the top right side of the plot to show different plot types."),
+                 br(),
+                 htmlOutput("evolu")), 
+                 
+        tabPanel('Scatter Plot, click Start', 
+                 helpText("This plot shows the evolution of species over time.
+                          Hover the mouse over the dots to show the dialect of the corresponding organism"),
+                 br(),
+                 plotlyOutput("scat_plotly"))
+      )
       #tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style2.css")),
       #tags$script(src="http://d3js.org/d3.v3.min.js"),
       #tags$script(src="showreel_d3.js"),
       #tags$div(id="d3outputplot"),
-      
-      p("Click on Start button: The plot below illustrates the number of encounters for each organism. By hoovering on the points you can see the identity (assign by position in linear space), and the number of encounters"),
-      plotlyOutput("encounter")
       )
     )
   )
