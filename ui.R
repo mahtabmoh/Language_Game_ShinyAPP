2b6496254a388aaa02f88988cfb09bf11db17957
Mahtab MOHAMMADI <mahtab.moh@gmail.com>
2017-03-27 07:43:28 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -26,11 +26,11 @@ shinyUI(fluidPage(
       actionButton("start", label = h5("Start")),
       actionButton("reset", label = h5("Reset")),
       sliderInput("n", label = h5("Number of organisms"), min = 50, max = 200, value = 50,step = 50),
-      sliderInput("iter", label = h5("Number of Iterations"), min = 10, max = 500,value = 50, step = 10),
+      sliderInput("iter", label = h5("Number of Cycles"), min = 10, max = 500,value = 100, step = 10),
       sliderInput("memspan", label = h5("Memory Span"),min = 1,max = 50,value = 5, step = 5),
-      sliderInput("dial_change_rate", label = h5("Dialect Update Probability (percentage)"), min = 0, max = 100, value = 1, step= 10),
-      sliderInput("repr_rate", label = h5("Reproduction Rate"), min= 1, max= 50, value = 10, step = 5),
-      sliderInput("Beta", label = h5("Decay Factor"), min = 1, max = 1.05, value = 1.029), step = 0.005),
+      sliderInput("dial_change_rate", label = h5("Dialect Update Probability (A percentage indicating the probability that fluctuations affect the dialects)"), min = 0, max = 100, value = 1, step= 10),
+      sliderInput("repr_rate", label = h5("Replication and Death Rate"), min= 1, max= 50, value = 10, step = 5),
+      sliderInput("Beta", label = h5("Decay Factor (for implementation of the encounter probability)"), min = 1, max = 1.05, value = 1.029), step = 0.005),
     
     # Create the main panel in which the plots are rendered
     # Each plot is rendered in a seperate tab
@@ -39,28 +39,28 @@ shinyUI(fluidPage(
       position = "left",
       h3("About the Game"),
       h4("This game simulates interactions between the members of a population of organisms that are assigned some randome characteristics.
-        Organims are distributed randomly on a linear space, and interact with their neighbours.
-        The nature of the interactions is defined by the strategies of organisms."),
+        Organims are distributed randomly in a linear space, and interact with their neighbours.
+        The nature of the interactions is defined by the strategies of organisms. A group of organisms belonging to the same strategy is referred to as "Species"."),
       h5("(Navigate between the tabs to view static/dynamic plots.)"),
       h5("(Due to the large size of the output dataset rendering might take several minutes.)"),
       tabsetPanel(
-        tabPanel('Encounters Plot, click Start',
-                 helpText("Hover over each point to show the ID of the corresponding organism"),br(),
+        tabPanel('Encounters Plot, click "Start"',
+                 helpText("Hover over each point to show the ID and the position of the corresponding organism in space"),br(),
                  plotlyOutput("encounter")),
                 
-        tabPanel('gvis Motion Chart, click Start',
-                 helpText("This plot animates the abundance of the species over cycles and the convergence
-                          of the population towards one strategy.
-                          Adjust the speed of motion next to play button. Select x-axis, y-axis and 
+        tabPanel('gvis Motion Chart, click "Start"',
+                 helpText("This plot animates the abundance of the species over cycles and observes the convergence
+                          of the population.
+                          Adjust the speed of motion next to the play button. Select x-axis, y-axis and 
                           size variable. Select log or lin for x or y-axis variable. Select unique colors from the color
                           slide bar to distinct the colors of each species.Navigate between
                           tabs at the top right side of the plot to show different plot types."),
                  br(),
                  htmlOutput("evolu")), 
                  
-        tabPanel('Scatter Plot, click Start', 
+        tabPanel('Scatter Plot, click "Start"', 
                  helpText("This plot shows the evolution of species over time.
-                          Hover the mouse over the dots to show the dialect of the corresponding organism"),
+                          Hover the mouse over the dots to show the dialect and the species of the corresponding organism"),
                  br(),
                  plotlyOutput("scat_plotly"))
       )
